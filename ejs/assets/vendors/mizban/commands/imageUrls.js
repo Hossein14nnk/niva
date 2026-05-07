c0a5f356159e1d3b7ce92bd13267d5f846980fde
hosseinka6566 <hosseinka6566@gmail.com>
2026-05-07 09:32:06 +0330

    add ejs folder

---
+++ ejs/assets/vendors/mizban/commands/imageUrls.js
@@ -0,0 +1,6 @@
+const imageUrls = [
+  "./assets/media/images/about_atr.webp",
+  "./assets/media/images/icons8-cursor-in-window-48.webp",
+  "./assets/media/images/IMG_20241108_164752_496.webp"
+];
+export default imageUrls;
