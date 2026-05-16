7f31a4818b25c7c2f1601302245c07ab830ab694
John Doe <johndoe@example.com>
2026-05-16 16:39:16 +0330

    end

--- ejs/output/fa/components/feature-description-box/script.js
+++
@@ -1,8 +0,0 @@
-let featureDescriptionBoxes = document.querySelectorAll(".feature-description-box");
-
-featureDescriptionBoxes.forEach(featureDescriptionBox => {
-    let description = featureDescriptionBox.querySelector(".description");
-    description.addEventListener("click", () => {
-        description.classList.remove("ellipsis-4");
-    });
-});
